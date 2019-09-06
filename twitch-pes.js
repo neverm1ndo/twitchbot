d3a18ef441f7efcb130b46bbdd07a7f6b9fe26e1
nmnd <reddigitalsw@gmail.com>
2019-09-06 15:41:14 +0300

    first commit

---
+++ twitch-pes.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const TwitchBot = require('twitch-bot')
+
+let conf = {
+  interval : 600000, // глобальный кулдаун автоматического постинга ссылок <- 10min
+  delay : 120000, // делей между различными ссылками <- 2 min
+  links : {
+    vk: 'https://vk.com/necessaryevil0',
+    donationalerts: 'https://www.donationalerts.com/r/necessaryevil0',
+    site: 'https://necessaryboost.pro/',
+    dotabuff: 'https://www.dotabuff.com/players/120494497',
+  }
+};
+const Bot = new TwitchBot({
+  username: 'evilsobakabot',
+  oauth: 'oauth:qg834r817uk9c9vc7vqhm46vaej8td',
+  channels: ['necessaryevil0']
+})
+
+function links() {
+  Bot.say(`DOTABUFF: ${conf.links.dotabuff} || VK: ${conf.links.vk} || Узнать цены на буст: ${conf.links.site}`)
+};
+
+function $timeout(message, index) {
+    setTimeout(()=> {
+      Bot.say(message);
+      console.log(` >BOT | AutoMessage #${index} : ${message}`);
+    }, conf.delay*index);
+}
+
+function autoPost() {
+  setInterval(()=> {
+    links();
+    $timeout('Подробный гайд от бустера на Ember Spirit: https://vk.com/@necessaryevil_boost-ember-spirit-kratkii-ekskurs-v-mir-legkih-25', 1);
+    $timeout('Почему стоит заказывать буст: https://vk.com/@necessaryevil_boost-bust-reitinga-mify-i-pravda', 2);
+    $timeout(`Поддержи стримлера: ${conf.links.donationalerts}`, 3);
+  }, conf.interval);
+}
+
+Bot.on('join', channel => {
+  console.log(`Joined channel: \x1b[30m\x1b[42m${channel}\x1b[0m`);
+  console.log(`> Start at ${new Date()}`);
+  autoPost();
+});
+
+Bot.on('error', err => {
+  console.log('\x1b[31m' + err + '\x1b[0m');
+})
+
+Bot.on('message', chatter => {
+  switch (chatter.message) {
+    case '!info':
+        links();
+      break;
+    case '!mmr':
+      Bot.say('Текущий MMR на мейне: 6200 OhMyDog')
+      break;
+    case '!donate':
+      Bot.say(`OhMyDog Поддержи стримлера: ${conf.links.donationalerts}`)
+      break;
+    case '!info':
+        links();
+      break;
+  }
+});
+Bot.on('subscription', event => {
+  Bot.say(`${event.login}, спасибо за подписку, братик! PogChamp`);
+})
