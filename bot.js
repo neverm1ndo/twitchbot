b451a8473b541a19027d8430f12f1e7899903ad1
neverm1ndo <reddigitalsw@gmail.com>
2019-09-18 16:24:36 +0300

    make modules more flexible

--- bot.js
+++ bot.js
@@ -8,12 +8,15 @@ const Player = require('./lib/player.module.js');
 const Party = require('./lib/party.module.js');
 const Manual = require('./lib/manual.module.js');
 const Loader = require('./lib/loader.module.js');
+const RNG = require('./lib/rng.module.js');
+const Bark = require('./lib/bark.module.js');
+const Stream = require('./lib/stream.module.js');
 
 const TwitchBot = require('twitch-bot')
 const request = require('request');
 const conf = require('./configs/bot.config.js');
 
-let stream, _stream, partyGathering, party, manual;
+let partyGathering, party, manual;
 let botStartDate = new Date();
 let loader = new Loader();
 let environment = JSON.parse(fs.readFileSync("environment.json"));
@@ -22,83 +25,15 @@ const Bot = new TwitchBot(environment.bot);
 
 const dictionary = JSON.parse(fs.readFileSync("./etc/banned.words.dict.json")).words;
 const sounds = JSON.parse(fs.readFileSync("./etc/sounds.library.json"));
+const automessages = JSON.parse(fs.readFileSync("./etc/automessages.list.json")).mod;
 
-function links() {
-  Bot.say(`DOTABUFF: ${conf.links.dotabuff} ||| VK: ${conf.links.vk} ||| Узнать цены на буст: ${conf.links.site}`)
-};
+const bark = new Bark(conf, automessages, Bot);
+const stream = new Stream({api: conf.api, headers: conf.headers});
 
-function $timeout(message, index) {
-    setTimeout(()=> {
-      Bot.say(message);
-      // console.log(`> BOT | AutoMessage #${index} ${Timestamp.stamp()}: ${message}`);
-    }, conf.delay*index);
-}
+//*************************************************************************************************************//
 
-function checkPrevilegies(chatter) {
+function CheckPrevilegies(chatter) {
   return (chatter.mod || (chatter.badges.broadcaster !== null));
-}
-
-function autoPost() {
-  setInterval(()=> {
-    links();
-    $timeout('OhMyDog Подробный гайд от бустера на Ember Spirit: https://vk.com/@necessaryevil_boost-ember-spirit-kratkii-ekskurs-v-mir-legkih-25', 1);
-    $timeout(`OhMyDog Поддержи стримлера: ${conf.links.donationalerts}`, 2);
-  }, conf.interval);
-}
-
-async function uptime() {
-  if (!stream) {
-    getStreamInfo();
-  }
-  return await new Promise((resolve, reject) => {
-    let start = stream.started_at;
-    let range = new Date(Date.now() - Date.parse(start));
-    if (stream) {
-      console.log(`> BOT | Success !uptime request: \x1b[32m${Timestamp.parse(range)}\x1b[0m | Данные получены с сервера Twitch`);
-      resolve(`OhMyDog Стрим идет уже ${Timestamp.parse(range)}`);
-    } else {
-      reject();
-    }
-  }).then(resolve => { Bot.say(resolve) })
-    .catch(err => {
-      console.log(`> BOT | Error !uptime request: \x1b[31m${err.message}\x1b[0m | Countdown from the start of the bot started...`);
-      Bot.say(`OhMyDog Стрим идет ${Timestamp.parse(Date.now() - Date.parse(botStartDate))} `);
-     });
-}
-
-function roll() {
-  return Math.floor(Math.random()*100);
-};
-
-function getStreamInfo() {
-  _stream = new Promise((resolve, reject) => {
-    process.stdout.write(`> BOT | Pending stream info from Twitch.tv ... `);
-    let options = {
-      url: conf.api.url + conf.api.id,
-      method: 'GET',
-      headers: conf.headers
-    };
-    request(options,(error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        let data = JSON.parse(body).data;
-        if (data[0]) {
-          resolve(data[0]);
-        } else {
-          reject();
-        }
-      } else {
-        reject();
-        }
-      })
-  }).then(res => {
-    stream = res ;
-    console.log('| \x1b[32m\x1b[1mSUCCESS!\x1b[0m');
-    console.log( `      └───> \x1b[32mAll stream info successfully received\x1b[0m\n`);
-    Table.build(res);
-  }).catch((err)=> {
-    console.log( `| \x1b[31m\x1b[1mERROR\x1b[0m`);
-    console.log( `      └───> \x1b[31mStream is offline or just started\x1b[0m\n`);
-  });
 }
 
 Bot.on('join', channel => {
@@ -107,8 +42,8 @@ Bot.on('join', channel => {
   console.log(`> Start at \x1b[1m${Timestamp.stamp()}\x1b[0m`);
   console.log(`> Manual mode ${conf.manual ? '\x1b[1m\x1b[33menabled\x1b[0m!': 'disabled'}`);
   console.log(`> Player : \x1b[1m${conf.player.type}\x1b[0m\n`)
-  autoPost();
-  getStreamInfo();
+  bark.start();
+  stream.info();
 });
 
 Bot.on('error', err => {
@@ -116,13 +51,13 @@ Bot.on('error', err => {
   Table.build(err);
 })
 
-Bot.on('message', chatter => {
+Bot.on('message', async chatter => {
   if (partyGathering) {
     if (chatter.message == '+') {
       party.gathering(chatter);
     }
   }
-  if (!checkPrevilegies(chatter)) {
+  if (!CheckPrevilegies(chatter)) {
     dictionary.forEach((word)=> {
       if (chatter.message.includes(word)) {
         console.warn(`> BOT | Catched banned word \x1b[1m\x1b[31m${word}\x1b[0m!\n      └───> Banned user \x1b[1m\x1b[31m${chatter.username}\x1b[0m`);
@@ -138,7 +73,7 @@ Bot.on('message', chatter => {
     }
   }
   if (chatter.message.includes('!party')) {
-    if (checkPrevilegies(chatter) && !partyGathering) {
+    if (CheckPrevilegies(chatter) && !partyGathering) {
       let amount = chatter.message.split(/\s/)[1];
       if (!amount) amount = 1;
       Bot.say(`OhMyDog @${chatter.username} собирает пати! Пиши + в чай, если хотите попасть в стак!`);
@@ -158,13 +93,13 @@ Bot.on('message', chatter => {
       Bot.say(`OhMyDog Поддержи стримлера: ${conf.links.donationalerts}`)
       break;
     case '!roll':
-      Bot.say(`${chatter.username} нароллил: ` + roll() + ' BlessRNG');
+      Bot.say(`${chatter.username} нароллил: ${RNG.randomize(0, 100)} BlessRNG`);
       break;
     case '!uptime':
-        uptime();
+      Bot.say(await stream.uptime());
       break;
     case '!s':
-      if (checkPrevilegies(chatter) && partyGathering) {
+      if (CheckPrevilegies(chatter) && partyGathering) {
         partyGathering = false;
         Bot.say(party.stack());
       };
@@ -174,6 +109,7 @@ Bot.on('message', chatter => {
 
 Bot.on('subscription', event => {
   Bot.say(`${event.login}, спасибо за подписку, братик! PogChamp`);
+  Table.build(event, true);
 });
 
 Bot.on('ban', event => {
@@ -191,7 +127,7 @@ if (conf.manual) {
     } else if (c.includes('$help')) {
       manual.help()
     } else if (c.includes(`$streamstatus`)) {
-      getStreamInfo();
+      stream.info();
     } else {
       manual.error();
     }
