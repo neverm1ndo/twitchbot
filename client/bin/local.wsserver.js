e6d9da437b9c9b19c705799307543cc9f8277167
neverm1ndo <reddigitalsw@gmail.com>
2019-09-21 18:27:28 +0300

    configure http ws client/server connection

--- client/bin/local.wsserver.js
+++ client/bin/local.wsserver.js
@@ -1,12 +1,49 @@
 'use strict'
 
 const WebSocket = require('ws');
+const http = require('http');
+const url = require('url');
 
-const wss = new WebSocket.Server({ port: 3000 })
+const fs = require('fs');
+const Table = require('../../lib/table.module.js');
+
+let conf = require('../../configs/bot.config.js');
+
+let server, index;
+
+fs.readFile('./client/client.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else {
+      index = html;
+    }
+});
+server = http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.end();
+})
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws, req) => {
+  console.log('Connected:', req.connection.remoteAddress);
   ws.on('message', message => {
     console.log(`Received message from ${req.connection.remoteAddress} => ${message}`)
   })
   ws.send('WS: Successfully conected to local WS server')
-})
+});
+
+server.listen(3000);
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  const pathname = url.parse(request.url).pathname;
+
+  if (pathname === '/foo') {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss1.emit('connection', ws, request);
+    });
+  } else{
+     socket.destroy();
+  }
+});
